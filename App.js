fa7f996e84f2d7d39a527f81f962deac3f9c3801
Jigme Lobsang lepcha <jigme16045@iiitd.ac.in>
2018-05-09 17:13:18 +0530

    Second Commit

--- App.js
+++ App.js
@@ -1,3 +1,16 @@
+/*
+    @author: Jigme Lobsang Lepcha
+
+    Assuming npm is already installed install React Native by typing in the command npm install -g create-react-native-app
+
+    To create a project use the command creat-react-native-app <Project Name>
+    cd <Project Name>
+    npm start
+
+    Install Expo app and scan the QR code to view the app live.
+*/
+
+
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
@@ -12,21 +25,25 @@ export default class App extends React.Component {
     return <Text> Inside the Text function {this.state.counter}</Text>  // We can define methods like this also.
   }
 
-  updateCounter(){
-    this.setState({counter: 2});  // Updates the state's value.
-  }
-
   render() {
     let pic = {uri: 'https://bit.ly/2jHF5lz'};  // Let makes the pic be visible only inside the render().
 
-    return (
+    return( 
       <View style={styles.container}>
         <Image source={pic} style = {{height: 100, width: 200}}/>
         <Text>Hi this is my first React Native App.</Text>
         <Text>The counter's value is {this.state.counter}</Text>
         {this.text()}
-        <Text> This is the updated counter's value {this.state.counter}</Text>
       </View>
+    );  // This needs to be called when you need to render your component on screen.
+    // Source is a prop.
+  }
+}
+
+class NewApp extends React.Component {
+  render(){
+    return(
+        <Text>Hello {this.props.name}!</Text>
     );
   }
 }
